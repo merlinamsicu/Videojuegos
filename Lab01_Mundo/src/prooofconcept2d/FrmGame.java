f4a92de2c68acea4f72f5ceec1a7341da3eac853
merlinamsicu <eli_mar52@hotmail.com>
2014-10-25 01:12:23 -0500

    subirlab1
    Signed-off-by: merlinamsicu <eli_mar52@hotmail.com>

---
+++ Lab01_Mundo/src/prooofconcept2d/FrmGame.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prooofconcept2d;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author servkey
+ */
+public class FrmGame extends JFrame{
+    private Thread t;
+    private CanvasDib c;
+    public FrmGame(CanvasDib c){
+        this.c = c;
+        add(c);
+        //Iniciar hilo
+        t = new Thread(){
+              public void run(){
+                  updating();
+              }
+        };
+        t.start();
+     
+        this.getContentPane().setBackground(Color.green);
+    }
+    
+    public void updating(){
+        while (true){
+            try {
+                Thread.sleep(100);
+                c.repaint();
+               
+            } catch (InterruptedException ex) {
+                Logger.getLogger(FrmGame.class.getName()).log(Level.SEVERE, null, ex);
+            }           
+        }
+    }
+}
